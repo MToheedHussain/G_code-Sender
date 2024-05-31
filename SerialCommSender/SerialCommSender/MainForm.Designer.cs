ceaf073120fffad28745a7e64dd04205dee7ce11
mtoheedhussain <toheed2011@gmail.com>
2024-05-31 15:51:58 +0500

    Add files via upload

---
+++ SerialCommSender/SerialCommSender/MainForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Serial
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cboxComport = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cboxBaudrate = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tboxData = new System.Windows.Forms.TextBox();
+            this.tboxReceive = new System.Windows.Forms.TextBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.btnNumberOfLines = new System.Windows.Forms.Button();
+            this.btnSendGcode = new System.Windows.Forms.Button();
+            this.timerSendGcode = new System.Windows.Forms.Timer(this.components);
+            this.btnStopSending = new System.Windows.Forms.Button();
+            this.btnSendAllAtOnce = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.numericUpDownLines = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLines)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboxComport
+            // 
+            this.cboxComport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.cboxComport.FormattingEnabled = true;
+            this.cboxComport.Location = new System.Drawing.Point(12, 12);
+            this.cboxComport.Name = "cboxComport";
+            this.cboxComport.Size = new System.Drawing.Size(121, 21);
+            this.cboxComport.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(295, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(104, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // cboxBaudrate
+            // 
+            this.cboxBaudrate.FormattingEnabled = true;
+            this.cboxBaudrate.Location = new System.Drawing.Point(12, 39);
+            this.cboxBaudrate.Name = "cboxBaudrate";
+            this.cboxBaudrate.Size = new System.Drawing.Size(121, 21);
+            this.cboxBaudrate.TabIndex = 2;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(163, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(104, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(361, 197);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "SendLine";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // tboxData
+            // 
+            this.tboxData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxData.Location = new System.Drawing.Point(12, 95);
+            this.tboxData.Multiline = true;
+            this.tboxData.Name = "tboxData";
+            this.tboxData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tboxData.Size = new System.Drawing.Size(329, 259);
+            this.tboxData.TabIndex = 5;
+            // 
+            // tboxReceive
+            // 
+            this.tboxReceive.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxReceive.Location = new System.Drawing.Point(463, 93);
+            this.tboxReceive.Multiline = true;
+            this.tboxReceive.Name = "tboxReceive";
+            this.tboxReceive.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tboxReceive.Size = new System.Drawing.Size(310, 259);
+            this.tboxReceive.TabIndex = 6;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(163, 63);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(104, 23);
+            this.browse.TabIndex = 7;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(12, 66);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(121, 20);
+            this.textBoxFilePath.TabIndex = 8;
+            // 
+            // btnNumberOfLines
+            // 
+            this.btnNumberOfLines.Location = new System.Drawing.Point(295, 63);
+            this.btnNumberOfLines.Name = "btnNumberOfLines";
+            this.btnNumberOfLines.Size = new System.Drawing.Size(104, 23);
+            this.btnNumberOfLines.TabIndex = 10;
+            this.btnNumberOfLines.Text = "NumberOfLines";
+            this.btnNumberOfLines.UseVisualStyleBackColor = true;
+            this.btnNumberOfLines.Click += new System.EventHandler(this.BtnNumberOfLines_Click);
+            // 
+            // btnSendGcode
+            // 
+            this.btnSendGcode.Location = new System.Drawing.Point(361, 132);
+            this.btnSendGcode.Name = "btnSendGcode";
+            this.btnSendGcode.Size = new System.Drawing.Size(75, 23);
+            this.btnSendGcode.TabIndex = 11;
+            this.btnSendGcode.Text = "SendAuto";
+            this.btnSendGcode.UseVisualStyleBackColor = true;
+            this.btnSendGcode.Click += new System.EventHandler(this.BtnSendGcode_Click);
+            // 
+            // timerSendGcode
+            // 
+            this.timerSendGcode.Tick += new System.EventHandler(this.TimerSendGcode_Tick);
+            // 
+            // btnStopSending
+            // 
+            this.btnStopSending.Location = new System.Drawing.Point(361, 161);
+            this.btnStopSending.Name = "btnStopSending";
+            this.btnStopSending.Size = new System.Drawing.Size(75, 23);
+            this.btnStopSending.TabIndex = 12;
+            this.btnStopSending.Text = "StopAuto";
+            this.btnStopSending.UseVisualStyleBackColor = true;
+            this.btnStopSending.Click += new System.EventHandler(this.BtnStopSending_Click);
+            // 
+            // btnSendAllAtOnce
+            // 
+            this.btnSendAllAtOnce.Location = new System.Drawing.Point(361, 256);
+            this.btnSendAllAtOnce.Name = "btnSendAllAtOnce";
+            this.btnSendAllAtOnce.Size = new System.Drawing.Size(75, 23);
+            this.btnSendAllAtOnce.TabIndex = 13;
+            this.btnSendAllAtOnce.Text = "SendAll";
+            this.btnSendAllAtOnce.UseVisualStyleBackColor = true;
+            this.btnSendAllAtOnce.Click += new System.EventHandler(this.BtnSendAllAtOnce_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(728, 10);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 14;
+            this.btnAbout.Text = "About us";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.BtnAbout_Click);
+            // 
+            // numericUpDownLines
+            // 
+            this.numericUpDownLines.Location = new System.Drawing.Point(405, 63);
+            this.numericUpDownLines.Name = "numericUpDownLines";
+            this.numericUpDownLines.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownLines.TabIndex = 16;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 450);
+            this.Controls.Add(this.numericUpDownLines);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnSendAllAtOnce);
+            this.Controls.Add(this.btnStopSending);
+            this.Controls.Add(this.btnSendGcode);
+            this.Controls.Add(this.btnNumberOfLines);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.tboxReceive);
+            this.Controls.Add(this.tboxData);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cboxBaudrate);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cboxComport);
+            this.Name = "MainForm";
+            this.Text = "Serial V1.0";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLines)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboxComport;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cboxBaudrate;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tboxData;
+        private System.Windows.Forms.TextBox tboxReceive;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button btnNumberOfLines;
+        private System.Windows.Forms.Button btnSendGcode;
+        private System.Windows.Forms.Timer timerSendGcode;
+        private System.Windows.Forms.Button btnStopSending;
+        private System.Windows.Forms.Button btnSendAllAtOnce;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.NumericUpDown numericUpDownLines;
+    }
+}
+
---
+++ SerialCommSender/SerialCommSender/MainForm.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Serial
+{
+
+    public partial class MainForm : Form
+    {
+        #region Constant
+        private readonly int[] baudrate = { 9600, 19200, 38400, 115200, 230400, 460800, 921600, 3860000 };
+        #endregion
+
+        private readonly SerialPort Serial = new SerialPort();
+
+        #region Local Helpers
+        private void UpdateCOMPortList()
+        {
+            // Get all existing Com Port names
+            string[] Ports = System.IO.Ports.SerialPort.GetPortNames();
+            cboxComport.Items.Clear();
+            cboxBaudrate.Items.Clear();
+
+            // Append existing COM to the cboxComport list
+            foreach (var item in Ports)
+            {
+                cboxComport.Items.Add(item);
+            }
+
+            // Append possible Baudrate to the cboxBaudrate list
+            foreach (var baud in baudrate)
+            {
+                cboxBaudrate.Items.Add(baud.ToString());
+            }
+        }
+        #endregion
+
+        #region Delegates
+        public delegate void UPDATE_OUTPUT_TEXT(String Str);
+        public void UpdateOutputText(String Str)
+        {
+            tboxReceive.Text += Str;
+            tboxReceive.ScrollToCaret();
+        }
+        #endregion
+
+
+        #region Handlers
+        void SerialOnReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            String str = Serial.ReadExisting();
+
+            Invoke(new UPDATE_OUTPUT_TEXT(UpdateOutputText), str);
+        }
+        #endregion
+       
+        public MainForm()
+        {
+            InitializeComponent();
+          
+            tboxData.ScrollBars = ScrollBars.Both; // Add this line to enable scrollbars
+            tboxReceive.ScrollBars = ScrollBars.Both; // Add this line to enable scrollbars
+            tboxData.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            tboxReceive.Anchor = AnchorStyles.Top |  AnchorStyles.Bottom;
+
+        }
+
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            // If user click disconnect
+            if ("Disconnect" == btnConnect.Text.ToString())
+            {
+                if (true == Serial.IsOpen)
+                {
+                    Serial.Close();
+                }
+
+                btnConnect.Text = "Connect";
+                cboxComport.Enabled = true;
+                cboxBaudrate.Enabled = true;
+                btnRefresh.Enabled = true;
+
+                return;
+            }
+
+            // else we gonna open the desired COM port
+            // Get user comport from cbox
+            try
+            {
+                Serial.PortName = cboxComport.Text;
+            }
+            catch
+            {
+                MessageBox.Show("Error! No COM Port selected");
+                return;
+            }
+
+            // Get user baudrate from cbox
+            try
+            {
+                Serial.BaudRate = int.Parse(cboxBaudrate.Text.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Error! No Baudrate selected");
+                return;
+            }
+
+            // Serial Port Configuration
+            Serial.Parity = Parity.None;
+            Serial.DataBits = 8;
+            Serial.ReceivedBytesThreshold = 1;
+            Serial.StopBits = StopBits.One;
+            Serial.Handshake = Handshake.None;
+            Serial.WriteTimeout = 3000;
+
+            // Check if com port is opened by other application
+            if (false == Serial.IsOpen)
+            {
+                try
+                {
+                    // Com port available
+                    Serial.Open();
+                }
+                catch
+                {
+                    MessageBox.Show("The COM port is not accessible", "Error");
+                    return;
+                };
+
+
+                // double comform it is opened
+                if (true == Serial.IsOpen)
+                {
+                    btnConnect.Text = "Disconnect";
+                    cboxComport.Enabled = false;
+                    cboxBaudrate.Enabled = false;
+                    btnRefresh.Enabled = false;
+
+                    // Add callback handler for receiving
+                    Serial.DataReceived += new SerialDataReceivedEventHandler(SerialOnReceivedHandler);
+
+                }
+
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // We need to populate the lists during mainform is loading
+            UpdateCOMPortList();
+        }
+
+        private void BtnRefresh_Click(object sender,
+                                      EventArgs e)
+        {
+            // We need to update all lists again if user requested
+            UpdateCOMPortList();
+        }
+        
+        private string[] gcodeLines;
+        private int currentLineIndex;
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "G-code Files|*.gcode;*.txt|All Files|*.*",
+                Title = "Select a G-code File"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                textBoxFilePath.Text = selectedFilePath;
+
+                // Read the G-code lines into an array
+                gcodeLines = File.ReadAllLines(selectedFilePath);
+                currentLineIndex = 0;
+
+            }
+        }
+       
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            if (isSendingGcode)
+            {
+                MessageBox.Show("Sending data in progress. Please stop the current sending process before initiating a new one.", "Sending in Progress", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (gcodeLines == null)
+            {
+                MessageBox.Show("Please select a G-code file first.");
+                return;
+            }
+
+            if (null != Serial)
+            {
+                if (true == Serial.IsOpen)
+                {
+                    int linesToSend = Convert.ToInt32(numericUpDownLines.Value);
+
+                    if (linesToSend > 0)
+                    {
+                        int remainingLines = gcodeLines.Length - currentLineIndex;
+                        linesToSend = Math.Min(linesToSend, remainingLines);
+
+                        for (int i = 0; i < linesToSend; i++)
+                        {
+                            string line = gcodeLines[currentLineIndex];
+                            Serial.WriteLine(line);
+
+                            // Append the sent line to the tboxData TextBox
+                            tboxData.Text += line + Environment.NewLine;
+                            tboxData.SelectionStart = tboxData.Text.Length;
+                            tboxData.ScrollToCaret();
+                            Application.DoEvents(); // Allow the UI to update
+
+                            currentLineIndex++;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid number of lines.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("COM Port is not Opened");
+                }
+            }
+
+        }
+
+
+
+
+        private void BtnNumberOfLines_Click(object sender, EventArgs e)
+        {
+            // Show a prompt for the user to enter the number of lines to send
+            using (var prompt = new Form())
+            {
+                prompt.Text = "Enter Number of Lines";
+                prompt.StartPosition = FormStartPosition.CenterParent;
+
+                var label = new Label() { Left = 20, Top = 20, Text = "Number of Lines:" };
+                var numericUpDown = new NumericUpDown() { Left = 150, Top = 20, Width = 100 };
+                var confirmButton = new Button() { Text = "OK", Left = 150, Width = 100, Top = 50, DialogResult = DialogResult.OK };
+
+                prompt.Controls.Add(numericUpDown);
+                prompt.Controls.Add(confirmButton);
+                prompt.Controls.Add(label);
+
+                if (prompt.ShowDialog() == DialogResult.OK)
+                {
+                    numericUpDownLines.Value = numericUpDown.Value;
+                }
+            }
+        }
+        private int storedLineIndex;
+
+        private void BtnSendGcode_Click(object sender, EventArgs e)
+        {
+            if (gcodeLines == null)
+            {
+                MessageBox.Show("Please select a G-code file first.");
+                return;
+            }
+
+            if (null == Serial)
+            {
+                return;
+            }
+            if (true != Serial.IsOpen)
+            {
+                MessageBox.Show("COM Port is not Opened");
+            }
+            else
+            {
+                // Start sending G-code lines with a 10ms delay
+                isSendingGcode = true;
+                currentLineIndex = storedLineIndex; // Resume from stored index
+                timerSendGcode.Start();
+            }
+        }
+
+
+        private bool isSendingGcode = false;
+
+        private void TimerSendGcode_Tick(object sender, EventArgs e)
+        {
+            if (currentLineIndex < gcodeLines.Length && isSendingGcode)
+            {
+                string line = gcodeLines[currentLineIndex];
+                Serial.WriteLine(line);
+
+                // Append the sent line to the tboxData TextBox
+                tboxData.Text += line + Environment.NewLine;
+                tboxData.SelectionStart = tboxData.Text.Length;
+                tboxData.ScrollToCaret();
+                Application.DoEvents(); // Allow the UI to update
+
+                currentLineIndex++;
+
+                if (currentLineIndex >= gcodeLines.Length)
+                {
+                    // All lines have been sent
+                    isSendingGcode = false;
+                    MessageBox.Show("All lines have been sent.");
+                }
+            }
+        }
+
+        private void BtnStopSending_Click(object sender, EventArgs e)
+        {
+            // Stop sending G-code lines
+            isSendingGcode = false;
+            timerSendGcode.Stop();
+
+            // Store the current line index
+            storedLineIndex = currentLineIndex;
+        }
+
+
+        private void BtnSendAllAtOnce_Click(object sender, EventArgs e)
+        {
+            if (isSendingGcode)
+            {
+                MessageBox.Show("Sending data in progress. Please stop the current sending process before initiating a new one.", "Sending in Progress", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (gcodeLines == null)
+            {
+                MessageBox.Show("Please select a G-code file first.");
+                return;
+            }
+
+            if (null != Serial)
+            {
+                if (true == Serial.IsOpen)
+                {
+                    foreach (string line in gcodeLines)
+                    {
+                        Serial.WriteLine(line);
+
+                        // Append the sent line to the tboxData TextBox
+                        tboxData.Text += line + Environment.NewLine;
+                        tboxData.SelectionStart = tboxData.Text.Length;
+                        tboxData.ScrollToCaret();
+                        Application.DoEvents(); // Allow the UI to update
+                    }
+
+                    MessageBox.Show("All lines have been sent.");
+                }
+                else
+                {
+                    MessageBox.Show("COM Port is not Opened");
+                }
+            }
+        }
+
+        private void BtnAbout_Click(object sender, EventArgs e)
+        {
+            string aboutMessage = "Serial Communication Application\n"
+                                + "University of Engineering and Technology, Taxila.\n"
+                                + "Version: 1.0\n"
+                                + "Contribution by: [M. Toheed Hussain]\n"
+                                + "Purpose:\n[Part of 'Smart PCB Fabrication Machine' Final Year Project]\n"                            
+                                + "Final Year Project Group Members:\nHamid Mehmood\n20-ENC-05\nWaqas Ahmed\n20-ENC-10\nM. Toheed Hussain\n20-ENC-46\nAmir Khan\n20-ENC-51\n"
+                                + "Supervisor: Dr. Sajjad Ahmed\n";
+                                
+
+
+            MessageBox.Show(aboutMessage, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
